3f564b4d470a4894a5e3d045ecd97854254e56ac
evgenyk <evgeny@komarevtsev.net>
2024-09-24 23:33:11 +1000

    defined interface for kinde project

--- projectBundler/projectBundler.go
+++ projectBundler/projectBundler.go
@@ -1,1 +1,28 @@
 package project_bundler
+
+type (
+	ProjectConfiguration struct {
+		Version string `json:"version"`
+		RootDir string `json:"root_dir"`
+	}
+
+	KindeWorkflow struct {
+		WorkflowRootDirectory string `json:"workflow_root_directory"`
+	}
+	KindeWorkflows struct {
+		Workflows []KindeWorkflow `json:"workflows"`
+	}
+
+	KindeEnvironment struct {
+		Workflows KindeWorkflows `json:"workflows"`
+	}
+
+	KindeProject struct {
+		Configuration ProjectConfiguration `json:"configuration"`
+		Environment   KindeEnvironment     `json:"environment"`
+	}
+
+	ProjectBundler interface {
+		Discover() KindeProject
+	}
+)
