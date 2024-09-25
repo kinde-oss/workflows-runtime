900fc48b19d9a61b51828bb3ef66c2722ca2413c
evgenyk <evgeny@komarevtsev.net>
2024-09-25 14:38:59 +1000

    kinde project discovers the root configuration

--- projectBundler/projectBundler.go
+++ projectBundler/projectBundler.go
@@ -1,9 +1,19 @@
 package project_bundler
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+)
+
 type (
 	ProjectConfiguration struct {
-		Version string `json:"version"`
-		RootDir string `json:"root_dir"`
+		Version     string `json:"version"`
+		RootDir     string `json:"rootDir"`
+		AbsLocation string `json:"location"`
 	}
 
 	KindeWorkflow struct {
@@ -22,7 +32,67 @@ type (
 		Environment   KindeEnvironment     `json:"environment"`
 	}
 
+	DiscoveryOptions struct {
+		StartFolder string
+	}
+
 	ProjectBundler interface {
-		Discover() KindeProject
+		Discover() (*KindeProject, error)
+	}
+
+	projectBundler struct {
+		options DiscoveryOptions
 	}
 )
+
+// Discover implements ProjectBundler.
+func (p *projectBundler) Discover() (*KindeProject, error) {
+	result := &KindeProject{}
+	configuration, err := discoverKindeRoot(p.options.StartFolder)
+	if err != nil {
+		return nil, fmt.Errorf("error discoving project root: %w", err)
+	}
+	result.Configuration = *configuration
+	return result, nil
+}
+
+func discoverKindeRoot(startFolder string) (*ProjectConfiguration, error) {
+
+	currentDirectory, _ := filepath.Abs(startFolder)
+
+	fileInfo, _ := os.Stat(currentDirectory)
+	if !fileInfo.IsDir() {
+		currentDirectory = filepath.Dir(currentDirectory)
+	}
+	for {
+		filePath := filepath.Join(currentDirectory, "kinde.json")
+		configFile, _ := os.Stat(filePath)
+		if configFile != nil {
+			return readProjectConfiguration(filePath)
+		}
+		parentPath := filepath.Join(currentDirectory, "..")
+		currentDirectory, _ = filepath.Abs(parentPath)
+		if currentDirectory == "" {
+			return nil, fmt.Errorf("could not find kinde.json")
+		}
+	}
+
+}
+
+func NewProjectBundler(discoveryOptions DiscoveryOptions) ProjectBundler {
+	return &projectBundler{
+		options: discoveryOptions,
+	}
+}
+
+func readProjectConfiguration(configFileInfo string) (*ProjectConfiguration, error) {
+	confiHandler, _ := os.Open(configFileInfo)
+	configFile, err := io.ReadAll(confiHandler)
+	if err != nil {
+		return nil, fmt.Errorf("error reading project configuration: %w", err)
+	}
+	result := &ProjectConfiguration{}
+	json.Unmarshal(configFile, result)
+	result.AbsLocation = path.Dir(configFileInfo)
+	return result, nil
+}
--- projectBundler/projectBundler_test.go
+++ projectBundler/projectBundler_test.go
@@ -1,6 +1,25 @@
 package project_bundler
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_ProjectBunler(t *testing.T) {
+	somePathInsideProject, _ := filepath.Abs("../testData/kindeSrc/environment/workflows") //starting in a middle of nowhere, so we need to go up to the root of the project
+
+	projectBundler := NewProjectBundler(DiscoveryOptions{
+		StartFolder: somePathInsideProject,
+	})
+
+	kindeProject, discoveryError := projectBundler.Discover()
+
+	assert := assert.New(t)
+
+	assert.Nil(discoveryError)
+	assert.Equal("2024-12-09", kindeProject.Configuration.Version)
+	assert.Equal("kindeSrc", kindeProject.Configuration.RootDir)
+
 }
