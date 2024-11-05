2f42ef24d66d54232ae034b42ca16326c938bf00
evgenyk <evgeny@komarevtsev.net>
2024-11-05 16:50:31 +1100

    added url and util

--- testData/kindeSrc/environment/workflows/evTest/tokensWorkflow.ts
+++ testData/kindeSrc/environment/workflows/evTest/tokensWorkflow.ts
@@ -5,6 +5,8 @@ export const workflowSettings = {
     trigger: 'onTokenGeneration',
     bindings:{
         "console": {},
+        "url": {},
+        "util": {},
         "kinde.fetch": {},
         "kinde.idToken": {
             resetClaims: true
@@ -16,10 +18,23 @@ export const workflowSettings = {
 };
 
 export default async function handle (event: any) {
-        kinde.idToken.setCustomClaim('random', 'test');
-        kinde.accessToken.setCustomClaim('test2', {"test2": hello});
-        console.log('logging from action', {"balh": "blah"});
-        await kinde.fetch("http://google.com");
-        console.error('error log');
-        return 'testing return';
+    const ps = new URLSearchParams({"aaa": "bbb", "ccc": "ddd"});
+    console.log('logging url search params', ps.toString());
+    if (ps.toString() !== 'aaa=bbb&ccc=ddd') {
+        throw new Error('URLSearchParams failed');
+    }
+
+    var txt = 'Congratulate %s on his %dth birthday!';
+    var result = util.format(txt, 'Ev', 42);
+    if (result !== 'Congratulate Ev on his 42th birthday!') {
+        throw new Error('util.format failed');
+    }
+    console.log('logging with util formatting', result);
+
+    kinde.idToken.setCustomClaim('random', 'test');
+    kinde.accessToken.setCustomClaim('test2', {"test2": hello});
+    console.log('logging from action', {"balh": "blah"});
+    await kinde.fetch("http://google.com");
+    console.error('error log');
+    return 'testing return';
 }
